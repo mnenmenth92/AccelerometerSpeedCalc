77e748a5f3b94d08c47202ff2f89a0cf94ed52ba
Maciej Such <msuch92@gmail.com>
2021-07-11 22:35:25 +0200

    first commit, data acquired from sensor, high pass filter to remove constant gravity factor

---
+++ example.py
@@ -0,0 +1,11 @@
+import time
+from lsm303d import LSM303D
+
+lsm = LSM303D(0x1e)  # Change to 0x1e if you have soldered the address jumper
+
+while True:
+    xyz = lsm.accelerometer()
+
+    print(("{:+06.2f}g : {:+06.2f}g : {:+06.2f}g").format(*xyz))
+
+    time.sleep(1.0/50)
---
+++ high_pass_filter.py
@@ -0,0 +1,24 @@
+import time
+from lsm303d import LSM303D
+
+lsm = LSM303D(0x1e)  # Change to 0x1e if you have soldered the address jumper
+alpha = 0.8
+x = y = z = 0
+
+
+while True:
+    xyz = lsm.accelerometer()
+
+    x = alpha * x + (1 - alpha) * xyz[0]
+    y = alpha * y + (1 - alpha) * xyz[1]
+    z = alpha * z + (1 - alpha) * xyz[2]
+
+    xl = xyz[0] - x
+    yl = xyz[1] - y
+    zl = xyz[2] - z
+
+
+    print(("{:+06.2f}g : {:+06.2f}g : {:+06.2f}g").format(xl, yl, zl))
+
+
+    time.sleep(1.0/50)
